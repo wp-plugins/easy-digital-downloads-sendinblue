881562d98db46eb7e2abded8e2cbc6d18cc455fc
codeinwp <codeinwp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-21 10:05:53 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/easy-digital-downloads-sendinblue/trunk@918573 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ edd-sendinblue.php
@@ -0,0 +1,128 @@
+<?php
+/*
+Plugin Name: Easy Digital Downloads - Sendinblue
+Plugin URL: http://easydigitaldownloads.com/extension/sendinblue
+Description: Include a Sendinblue signup option with your Easy Digital Downloads checkout
+Version: 1.0
+Author: ThemeIsle
+Author URI: http://themeisle.com
+Contributors: themeisle
+*/
+
+// adds the settings to the Misc section
+function eddsendinblue_add_settings($settings) {
+  
+  $eddsendinblue_settings = array(
+		array(
+			'id' => 'eddsendinblue_settings',
+			'name' => '<strong>' . __('Sendinblue Settings', 'eddsendinblue') . '</strong>',
+			'desc' => __('Configure sendinblue Integration Settings', 'eddsendinblue'),
+			'type' => 'header'
+		),
+        array(
+			'id' => 'eddsendinblue_access_key',
+			'name' => __('Acces Key', 'eddsendinblue'),
+			'desc' => __('Enter your sendinblue access Key. It is located in the API & Integration area of your sendinblue account.', 'eddsendinblue'),
+			'type' => 'text',
+			'size' => 'regular'
+		),
+		array(
+			'id' => 'eddsendinblue_secret_key',
+			'name' => __('Secret Key', 'eddsendinblue'),
+			'desc' => __('Enter your sendinblue Secret Key. It is located in the API & Integration area of your sendinblue account.', 'eddsendinblue'),
+			'type' => 'text',
+			'size' => 'regular'
+		),
+		array(
+			'id' => 'eddsendinblue_list_id',
+			'name' => __('List ID', 'eddsendinblue'),
+			'desc' => __('Select your List ID. Note: this option appears only after you have entered and saved your Access Key in the field above.', 'eddsendinblue'),
+			'type' => 'select',
+			'options' => eddsendinblue_get_sendinblue_lists()
+		),
+		array(
+			'id' => 'eddsendinblue_label',
+			'name' => __('Checkout Label', 'eddsendinblue'),
+			'desc' => __('This is the text shown next to the signup option', 'eddsendinblue'),
+			'type' => 'text',
+			'size' => 'regular'
+		)
+	);
+	
+	return array_merge($settings, $eddsendinblue_settings);
+}
+add_filter('edd_settings_misc', 'eddsendinblue_add_settings');
+
+// get an array of all sendinblue subscription lists
+function eddsendinblue_get_sendinblue_lists() {
+	
+	global $edd_options;
+	
+	if( isset( $edd_options['eddsendinblue_access_key'] ) && strlen( trim( $edd_options['eddsendinblue_access_key'] ) ) > 0 ) {
+		
+		$lists = array();
+		if( !class_exists( 'Mailin' ) )
+            require_once('inc/sendinblue.class.php');
+		$api = new Mailin('https://api.sendinblue.com/v1.0',$edd_options['eddsendinblue_access_key'],$edd_options['eddsendinblue_secret_key']);
+		$retval = $api->get_lists();
+		$retval = $retval['data'];
+		if($retval) :
+			foreach($retval as $list) :
+				$lists[$list['id']] = $list['name'];
+			endforeach;
+		endif;
+		return $lists;
+	}
+	return array();
+}
+    
+// adds an email to the sendinblue subscription list
+function eddsendinblue_subscribe_email($email, $first_name = 'xx', $last_name = '' ) {
+	global $edd_options;
+	
+	if( isset( $edd_options['eddsendinblue_access_key'] ) && strlen( trim( $edd_options['eddsendinblue_access_key'] ) ) > 0 ) {
+
+		if( ! isset( $edd_options['eddsendinblue_list_id'] ) || strlen( trim( $edd_options['eddsendinblue_list_id'] ) ) <= 0 )
+			return false;
+        
+        require_once('inc/sendinblue.class.php');
+
+        $api = new Mailin('https://api.sendinblue.com/v1.0',$edd_options['eddsendinblue_access_key'],$edd_options['eddsendinblue_secret_key']);
+        
+        $id = array($edd_options['eddsendinblue_list_id']);
+
+		$attributes = array("NAME"=>$first_name, "SURNAME"=>$last_name); // name of attribute should be present in your acount
+		$blacklisted = 0;
+		$listid_unlink = array();
+		$retval=$api->create_update_user($email,$attributes,$blacklisted,$id,$listid_unlink);
+      
+        
+	}
+
+	return false;
+}
+
+// displays the sendinblue checkbox
+function eddsendinblue_sendinblue_fields() {
+	global $edd_options;
+	ob_start(); 
+		if( isset( $edd_options['eddsendinblue_access_key'] ) && strlen( trim( $edd_options['eddsendinblue_access_key'] ) ) > 0 ) { ?>
+		<p>
+			<input name="eddsendinblue_sendinblue_signup" id="eddsendinblue_sendinblue_signup" type="checkbox" checked="checked"/>
+			<label for="eddsendinblue_sendinblue_signup"><?php echo isset($edd_options['eddsendinblue_label']) ? $edd_options['eddsendinblue_label'] : __('Sign up for our mailing list', 'eddsendinblue'); ?></label>
+		</p>
+		<?php
+	}
+	echo ob_get_clean();
+}
+add_action('edd_purchase_form_before_submit', 'eddsendinblue_sendinblue_fields', 100);
+
+// checks whether a user should be signed up for the sendinblue list
+function eddsendinblue_check_for_email_signup($posted, $user_info) {
+	if($posted['eddsendinblue_sendinblue_signup']) {
+
+		$email = $user_info['email'];
+		eddsendinblue_subscribe_email($email, $user_info['first_name'], $user_info['last_name'] );
+	}
+}
+add_action('edd_checkout_before_gateway', 'eddsendinblue_check_for_email_signup', 10, 2);
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Plugin Name ===
+Contributors: codeinwp
+Tags:  sendinblue,edd,easydigitaldownloads
+Author URI: http://codeinwp.com 
+Requires at least: 3.5
+Tested up to: 3.9
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ 
+
+== Description ==
+
+Include a Sendinblue signup option with your Easy Digital Downloads checkout
+
+== Installation ==
+
+Following are the steps to install the Easy Digital Downloads - Sendinblue
+
+1. Download the latest version of the Easy Digital Downloads - Sendinblue to your computer from here.
+2. With an FTP program, access your site?s server.
+3. Upload (copy) the Plugin file(s) or folder to the /wp-content/plugins folder.
+4. In your WordPress Administration Panels, click on Plugins from the menu.
+5. You should see Tweet Old Posts Plugin listed. If not, with your FTP program, check the folder to see if it is installed. If it isn?t, upload the file(s) again. If it is, delete the files and upload them again.
+6. To turn the WP Product Review on, click Activate.
+7. Check your Administration Panels or WordPress blog to see if the Plugin is working.
+8. You can change the plugin options from Tweet Old Posts under settings menu.
+
+Alternatively you can also follow the following steps to install the WEasy Digital Downloads - Sendinblue
+
+1. In your WordPress Administration Panels, click on Add New option under Plugins from the menu.
+2. Click on upload at the top.
+3. Browse the location and select the WP Product Review Plugin and click install now.
+4. To turn the WP Product Review Plugin on, click Activate.
+5. Check your Administration Panels or WordPress blog to see if the Plugin is working.
+6. You can change the plugin options from Tweet Old Posts under settings menu.
+
+== Frequently Asked Questions ==
+
+If you have any questions please get in touch with us at,
+http://themeisle.com/contact/
